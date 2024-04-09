117334ee5839a4e0287c6f02acdecce3b9bb106d
suyashkhanal <110769343+suyashkhanal@users.noreply.github.com>
2024-04-09 19:45:25 +0545

    Create 3.c

---
+++ lab set 1/3.c
@@ -0,0 +1,13 @@
+// program to compute floor valuse and ceiling value
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int f,p;
+	float a;
+	printf("Enter a decimal value: ");
+	scanf("%d",&a);
+    c=ceil(a);
+	f=floor(a);
+	printf("The floor value is %d and ceiling value is %d",f,c);
+}
